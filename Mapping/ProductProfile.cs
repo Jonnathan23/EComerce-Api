416d21fbd378926863d26d2ba3e95765e890ee8e
Jonnathan23 <jsaquicela26@gmail.com>
2026-08-13 19:15:50 -0500

    feat: add mapping and dtos for products

---
+++ Mapping/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace EComerce;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<Product, CreateProductDto>().ReverseMap();
+        CreateMap<Product, UpdateProductDto>().ReverseMap();
+    }
+}
